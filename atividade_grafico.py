8b1ac28ce63faf5dfca1582349f5b2ff5b626d16
Nicollas Correia <nicollas225@gmail.com>
2023-06-17 21:31:48 -0300

    changing chart appearance

--- atividade_grafico.py
+++ atividade_grafico.py
@@ -6,6 +6,11 @@ caminho_arquivo = "A:/Curso/Módulo_18/Atividade/gasolina.csv"
 df = pd.read_csv(caminho_arquivo)
 df.head()
 
-sns.lineplot(x="dia", y="venda", data=df)
+plt.style.use("ggplot")
+plt.figure(figsize=(15,5))
+sns.lineplot(data=df, x="dia", y="venda")
+plt.title("Histórico de vendas", fontweight="bold")
+plt.xlabel("Dia da venda")
+plt.ylabel("Valor vendido")
 
 plt.savefig("A:/Curso/Módulo_18/Atividade/grafico.png")
