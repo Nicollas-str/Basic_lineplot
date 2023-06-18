261add1c4e89047ec043b68747a7cfb3a40a85b5
Nicollas <nicollas225@gmail.com>
2023-06-17 21:50:16 -0300

    Update atividade_grafico.py

--- atividade_grafico.py
+++ atividade_grafico.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 caminho_arquivo = "A:/Curso/Módulo_18/Atividade/gasolina.csv"
 df = pd.read_csv(caminho_arquivo)
-df.head()
 
 plt.style.use("ggplot")
 plt.figure(figsize=(15,5))
